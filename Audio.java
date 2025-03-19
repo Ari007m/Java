05e13fb935949ce5b8498b3c07225d65d52255bc
ARIHARASUDAN M <127032340+Ari007m@users.noreply.github.com>
2025-03-19 13:36:16 +0530

    Add files via upload

---
+++ Audio.java
@@ -0,0 +1,58 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Audio {
+    public static void main(String[] args) {
+
+        String path = "C:\\Users\\ariha\\Downloads\\MATUSHKA PHONK [ MATUSHKA ZEMLYA BRAZILIAN FUNK ] (promodj.com).wav";
+        File file = new File(path);
+
+        try(Scanner s =new Scanner(System.in);AudioInputStream a = AudioSystem.getAudioInputStream(file)){
+            Clip c = AudioSystem.getClip();
+            c.open(a);
+            System.out.println("File opened");
+
+
+            String ip ="";
+            while (!ip.equals("Q")){
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset");
+                System.out.println("Q = Quit");
+                System.out.print("Enter your Choice: ");
+
+                ip = s.next().toUpperCase();
+
+                switch (ip){
+                    case "P" -> c.start();
+                    case "S" -> c.stop();
+                    case "R" -> c.setMicrosecondPosition(0);
+                    case "Q" -> c.close();
+                    default -> System.out.println("Invalid Choice");
+                }
+
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+
+        }
+        catch (UnsupportedAudioFileException e){
+            System.out.println("Audio type not supported");
+        }
+        catch (LineUnavailableException e){
+            System.out.println("Unable to access");
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong");
+        }
+        finally {
+            System.out.println("Done");
+        }
+    }
+
+}
+
---
+++ SlotMachine.java
@@ -0,0 +1,110 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class SlotMachine {
+    public static void main(String[] args){
+        /*
+        Java slot machine program
+
+        variable
+        welcome message
+        play bal>0
+        enter bet amount
+              verify bet > 0
+              verify bet < bal
+         spin
+         print row
+         payout
+         ask to play again
+         end note
+         */
+
+        Scanner s = new Scanner(System.in);
+        double bal = 100;
+        double bet ;
+        double won ;
+        String[] row ;
+        String play ;
+        System.out.println("WELCOME TO JAVA SLOT MACHINE");
+        System.out.println("Symbols: ⭐ ❤️ 🥳 👻 🤡  ");
+
+        while(bal > 0){
+            System.out.println("Your balance: ₹"+bal);
+            System.out.print("place your bet: ₹");
+            bet = s.nextDouble();
+            s.nextLine();
+            if(bet <= 0 || bet > bal){
+                System.out.println("INSUFFICIENT MONEY");
+                continue;
+            } else {
+                bal-=bet;
+            }
+            System.out.println("Spinning ........");
+            row = spin();
+            printRow(row);
+            won = payOut(row,bet);
+            if(won > 0){
+                System.out.println("You won: ₹"+won);
+                bal += won;
+            }
+            else {
+                System.out.println("Sorry you lost");
+            }
+            System.out.print("Do you want to play again(Y/N): ");
+            play =s.next().toUpperCase();
+            if(!play.equals("Y")){
+                break;
+            }
+        }
+        System.out.println("Game over !!! Your final balance: ₹"+bal);
+        s.close();
+
+    }
+    static String[] spin(){
+
+        String[] symbol = {"⭐","❤️","🥳","👻","🤡"};
+        String[] row = new String[3];
+        Random r = new Random();
+
+        for(int i= 0; i<3 ; i++){
+            row[i] = symbol[r.nextInt(symbol.length)];
+        }
+
+        return row;
+
+    }
+    static void printRow(String[] row){
+        System.out.println(" "+String.join(" | ",row));
+    }
+    static double payOut(String[] row,double bet){
+        if(row[0].equals(row[1]) && row[1].equals(row[2])){
+            return switch (row[0]){
+                case "⭐" -> bet*20;
+                case "❤️" -> bet*10;
+                case "🥳" -> bet*7;
+                case "👻" -> bet*5;
+                case "🤡" -> bet*2;
+                default -> 0;
+
+            };
+
+        }
+        else if(row[0].equals(row[1]) || row[1].equals(row[2])){
+            return switch (row[1]){
+                case "⭐" -> bet*10;
+                case "❤️" -> bet*7;
+                case "🥳" -> bet*5;
+                case "👻" -> bet*2;
+                case "🤡" -> bet*1;
+                default -> 0;
+
+            };
+
+        }
+
+        return 0;
+
+    }
+
+
+}
