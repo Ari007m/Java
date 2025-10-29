4eb5aac8dea9b828e4881c1b2581ee010fdfde47
Ari007m <ariharasudan007@gmail.com>
2025-10-29 20:13:24 +0530

    Thread name and interrupt added

--- src/process_thread_demo/MultiThreadUsingThread.java
+++ src/process_thread_demo/MultiThreadUsingThread.java
@@ -16,6 +16,10 @@ class Hii implements Runnable{
 
 class Hello extends Thread{
 
+    public Hello(String s) {
+        super(s);
+    }
+
     public void run() {
         for(int i=0;i<=20;i++){
             System.out.println("This is Hello");
@@ -32,20 +36,21 @@ public class MultiThreadUsingThread {
     public static void main(String[] args) throws InterruptedException {
 
         Hii jobOfHi = new Hii(); // Object of task which want to be executed.
-        Hello hello = new Hello();
+        Hello hello = new Hello("Hello-Thread");
 
-        Thread hi =new Thread(jobOfHi); // thread creation of the job
+        Thread hi =new Thread(jobOfHi,"Hi-Thread"); // thread creation of the job
 
         hi.start();
-        System.out.println("Hi started");
+        System.out.println(hi.getName() + " started");
         try {
             Thread.sleep(250);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         hello.start();
-        System.out.println("Hello started");
+        System.out.println(hello.getName() + " started");
 
+//        hi.interrupt(); // This interrupts the Sleep state of Hi thread
         hi.join(); // Main thread is waiting for 'hi' thread to finish its execution
         hello.join(); // Main thread is waiting for 'hello' thread to finish its execution
 
