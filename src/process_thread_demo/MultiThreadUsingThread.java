1643b17d76932a9a5cffc79ec86795bb9dd9d05f
Ari007m <ariharasudan007@gmail.com>
2025-10-29 18:14:41 +0530

    Programs comment added

--- src/process_thread_demo/MultiThreadUsingThread.java
+++ src/process_thread_demo/MultiThreadUsingThread.java
@@ -1,7 +1,7 @@
 package process_thread_demo;
 
 class Hii implements Runnable{
-
+// This Class implements runnable which has single method
     public void run() {
         for(int i=0;i<=30;i++){
             System.out.println("This is Hii");
@@ -31,10 +31,10 @@ class Hello extends Thread{
 public class MultiThreadUsingThread {
     public static void main(String[] args) throws InterruptedException {
 
-        Hii jobOfHi = new Hii();
+        Hii jobOfHi = new Hii(); // Object of task which want to be executed.
         Hello hello = new Hello();
 
-        Thread hi =new Thread(jobOfHi);
+        Thread hi =new Thread(jobOfHi); // thread creation of the job
 
         hi.start();
         System.out.println("Hi started");
@@ -44,10 +44,10 @@ public class MultiThreadUsingThread {
             throw new RuntimeException(e);
         }
         hello.start();
-        System.out.println("Hi started");
+        System.out.println("Hello started");
 
-        hi.join();
-        hello.join();
+        hi.join(); // Main thread is waiting for 'hi' thread to finish its execution
+        hello.join(); // Main thread is waiting for 'hello' thread to finish its execution
 
         System.out.println("Main waiting for all tasks to complete");
     }
